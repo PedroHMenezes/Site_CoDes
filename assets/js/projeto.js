90dda98abfac642df5b398fd298c365677ae66af
lucaskf1996 <lucaskf1996@gmail.com>
2019-09-16 21:22:14 -0300

    gambiarra menos gambiarra

--- assets/js/projeto.js
+++ assets/js/projeto.js
@@ -6,27 +6,14 @@ document.addEventListener('DOMContentLoaded', function() {
     let params = coDesExtract()
     //console.log(params)
     let value = params['key']
-    let categ = MapDataBase(data)
-    let categid = categ[value]
-    console.log(categid)
-    //console.log(value)
+    let categ = params['category']
+    console.log(categ)
+    console.log(value)
 
-  //console.log(data)
-    context = data['portfolio'][categid]['projetos'][value]
+    context = data['portfolio'][categ]['projetos'][value]
     console.log(context)
     coDesReplace('body',context)
 
 
  })
-
-function MapDataBase(object) {
-  map = {}
-  for(let directory in object["portfolio"]) {
-	   for (let project in object["portfolio"][directory]["projetos"]) {
-		     map[project] = directory;
-	   }
-  }
-  return map
-  }
-
 })
